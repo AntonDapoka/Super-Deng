d7e235e0a981ef23cb87c8e1907bc5ea69087d51
Anton Borisov <antonimstudio@gmail.com>
2025-02-10 17:02:37 +0800

    Some Bug Fix

--- Assets/Scripts/UIGameSceneScripts/BeatController.cs
+++ Assets/Scripts/UIGameSceneScripts/BeatController.cs
@@ -11,16 +11,7 @@ public class BeatController : MonoBehaviour //Скрипт под снос
     public bool canPress = false;
     public bool canCombo = false;
 
-    [SerializeField] private Image image1;
-    private Vector3 startPos1 = new Vector3(-350f, 0f, 0f);
-    private Vector3 midPos1 = new Vector3(-200f, 0f, 0f);
-    private Vector3 almostEndPos1 = new Vector3(-50f, 0f, 0f);
-    private Vector3 endPos1 = new Vector3(100f, 0f, 0f);
-    [SerializeField] private Image image2;
-    private Vector3 startPos2 = new(350f, 0f, 0f);
-    private Vector3 midPos2 = new(200f, 0f, 0f);
-    private Vector3 almostEndPos2 = new Vector3(50f, 0f, 0f);
-    private Vector3 endPos2 = new Vector3(-100f, 0f, 0f);
+
     public int beatcount = 0;
     private float lastBeatTime = 0f;
     public bool isAlreadyPressed = false;
@@ -29,80 +20,35 @@ public class BeatController : MonoBehaviour //Скрипт под снос
     private void Start()
     {
         beatInterval = RM.beatInterval;
-        if (!isTutorial)
-        {
-            image1.enabled = false;
-            image1.rectTransform.localPosition = startPos1;
-            image2.enabled = false;
-            image2.rectTransform.localPosition = startPos2;
-        }
     }
 
     private void Update()
     {
-        if (isTurnOn)
+        if (!isTurnOn) return;
+
+        canCombo = (elapsedTime > 0f) && ((elapsedTime < 0.25f * beatInterval) || (elapsedTime > 0.75f * beatInterval));
+
+        if (elapsedTime <= 0f) return;
+
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime < beatInterval / 3f)
         {
-            if (0f < elapsedTime && ((elapsedTime < 0.25f * beatInterval) || (elapsedTime > 0.75f * beatInterval)))
-            {
-                canCombo = true;
-            }
-            else canCombo = false;
-
-            if (elapsedTime > 0f)
-            {
-                elapsedTime += Time.deltaTime;
-
-                if (elapsedTime < beatInterval / 3f)
-                {
-
-                    if (!isAlreadyPressed)
-                        canPress = true;
-                    else
-                        canPress = false;
-                    float t = elapsedTime / (beatInterval / 3f);
-                    if (!isTutorial)
-                    {
-                        image1.rectTransform.localPosition = Vector3.Lerp(almostEndPos1, endPos1, t);
-                        image2.rectTransform.localPosition = Vector3.Lerp(almostEndPos2, endPos2, t);
-                    }
-                    
-
-                }
-                else if (elapsedTime < (2f * beatInterval) / 3f)
-                {
-                    canPress = false;
-
-
-                    if (!isAlreadyPressedIsAlreadyPressed)
-                        PressIsAlreadyPress();
-
-                    float t = (elapsedTime - (beatInterval / 3f)) / (beatInterval / 3f);
-                    if (!isTutorial)
-                    {
-                        image1.enabled = true;
-                        image1.rectTransform.localPosition = Vector3.Lerp(startPos1, midPos1, t);
-                        image2.enabled = true;
-                        image2.rectTransform.localPosition = Vector3.Lerp(startPos2, midPos2, t);
-                    }
-                }
-                else if (elapsedTime < beatInterval)
-                {
-                    if (!isAlreadyPressed)
-                        canPress = true;
-                    else
-                        canPress = false;
-                    float t = (elapsedTime - ((2f * beatInterval) / 3f)) / (beatInterval / 3f);
-                    if (!isTutorial)
-                    {
-                        image1.rectTransform.localPosition = Vector3.Lerp(midPos1, almostEndPos1, t);
-                        image2.rectTransform.localPosition = Vector3.Lerp(midPos2, almostEndPos2, t);
-                    }
-                }
-                else
-                {
-                    elapsedTime = 0f;
-                }
-            }
+            canPress = !isAlreadyPressed;
+        }
+        else if (elapsedTime < (2f * beatInterval) / 3f)
+        {
+            canPress = false;
+            if (!isAlreadyPressedIsAlreadyPressed)
+                PressIsAlreadyPress();
+        }
+        else if (elapsedTime < beatInterval)
+        {
+            canPress = !isAlreadyPressed;
+        }
+        else
+        {
+            elapsedTime = 0f;
         }
     }
 
--- Assets/Scripts/UIGameSceneScripts/BeatFlickeringScript.cs
+++ Assets/Scripts/UIGameSceneScripts/BeatFlickeringScript.cs
@@ -6,119 +6,38 @@ public class BeatFlickeringScript : MonoBehaviour
 {
     public bool isTurnOn = false;
     [SerializeField] private RhythmManager RM;
-    private float beatInterval;
-    private float elapsedTime = 0f;
-    public bool canPress = false;
-    public bool canCombo = false;
+    [SerializeField] private BeatController BC;
 
 
     public Material materialToFade;
 
-    public int beatcount = 0;
-    private float lastBeatTime = 0f;
-    public bool isAlreadyPressed = false;
-    public bool isAlreadyPressedIsAlreadyPressed = false;
+
 
     private void Start()
     {
-        beatInterval = RM.beatInterval;
-
-        SetMaterialAlpha(0f);
+        SetMaterialAlpha(1f);
     }
 
     private void Update()
     {
         if (isTurnOn)
         {
-            if (0f < elapsedTime && ((elapsedTime < 0.25f * beatInterval) || (elapsedTime > 0.75f * beatInterval)))
+            if (BC.canPress)
             {
-                canCombo = true;
-            }
-            else canCombo = false;
+                SetMaterialAlpha(0.25f);
+                if (BC.canCombo)
+                {
+                    SetMaterialAlpha(0f);
+                    Debug.Log("0f");
+                }
 
-            if (elapsedTime > 0f)
+            }
+            else
             {
-                elapsedTime += Time.deltaTime;
-
-                if (elapsedTime < beatInterval / 3f)
-                {
-
-                    if (!isAlreadyPressed)
-                        canPress = true;
-                    else
-                        canPress = false;
-                    float t = elapsedTime / (beatInterval / 3f);
-
-                    //float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, t);
-                    //SetMaterialAlpha(newAlpha);
-
-                }
-                else if (elapsedTime < (2f * beatInterval) / 3f)
-                {
-                    canPress = false;
-
-
-                    if (!isAlreadyPressedIsAlreadyPressed)
-                        PressIsAlreadyPress();
-
-                    float t = (elapsedTime - (beatInterval / 3f)) / (beatInterval / 3f);
-                    //image1.enabled = true;
-                    //image1.rectTransform.localPosition = Vector3.Lerp(startPos1, midPos1, t);
-                    //image2.enabled = true;
-                    //image2.rectTransform.localPosition = Vector3.Lerp(startPos2, midPos2, t);
-                }
-                else if (elapsedTime < beatInterval)
-                {
-                    if (!isAlreadyPressed)
-                        canPress = true;
-                    else
-                        canPress = false;
-                    float t = (elapsedTime - ((2f * beatInterval) / 3f)) / (beatInterval / 3f);
-                    //image1.rectTransform.localPosition = Vector3.Lerp(midPos1, almostEndPos1, t);
-                    //image2.rectTransform.localPosition = Vector3.Lerp(midPos2, almostEndPos2, t);
-                }
-                else
-                {
-                    elapsedTime = 0f;
-                }
+                SetMaterialAlpha(0.75f);
+                Debug.Log("1f");
             }
-        }
-    }
-
-    private void PressIsAlreadyPress()
-    {
-        isAlreadyPressed = false;
-        isAlreadyPressedIsAlreadyPressed = true;
-    }
-
-    public void OnBeat()
-    {
-        float currentTime = Time.time;
-        if (lastBeatTime != 0f)
-        {
-            beatInterval = currentTime - lastBeatTime;
-        }
-        lastBeatTime = currentTime;
-        beatcount++;
-        elapsedTime = 0.0001f;
-        //image.rectTransform.localPosition = startPos; // “елепортирование на начальную позицию
-    }
-    private IEnumerator FadeToAlpha(float targetAlpha, float duration)
-    {
-        Color color = materialToFade.color;
-        float startAlpha = color.a;
-        float timeElapsed = 0f;
-
-        while (timeElapsed < duration)
-        {
-            timeElapsed += Time.deltaTime;
-            float newAlpha = Mathf.Lerp(startAlpha, targetAlpha, timeElapsed / duration);
-            SetMaterialAlpha(newAlpha);
-            yield return null;
-        }
-
-        // ”бедимс€, что финальное значение точно установлено
-        SetMaterialAlpha(targetAlpha);
+        }    
     }
 
     private void SetMaterialAlpha(float alpha)
