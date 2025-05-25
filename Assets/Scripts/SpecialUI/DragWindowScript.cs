d2a5d7690785803aaa6a09ac3ed22c0cdf0fd96b
Anton Borisov <antonimstudio@gmail.com>
2025-05-25 14:57:47 +0800

    Window Added

---
+++ Assets/Scripts/SpecialUI/DragWindowScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindowScript : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragRectTransform.SetAsLastSibling();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
