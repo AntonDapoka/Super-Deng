642591a3a435ee45c25011c22eef2677ad9930a5
Anton Borisov <antonimstudio@gmail.com>
2025-11-18 17:04:37 +0800

    I need to work MORE

--- Assets/Scripts/Interactor/Field/Builders/FaceIcosphereBuilderScript.cs
+++ Assets/Scripts/Interactor/Field/Builders/FaceIcosphereBuilderScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FaceIcosphereBuilderScript : MonoBehaviour
+public class FaceIcosphereBuilderScript : FaceIcosahedronBuilderScript, IBuilderScript
 {
     // Start is called before the first frame update
     void Start()
