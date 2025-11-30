c4a2eb7fba6036e0bf15e3472ec587308ae80159
Anton Borisov <antonimstudio@gmail.com>
2025-11-30 16:40:00 +0800

    Taunt Ability

--- Assets/Scripts/Interactor/Abilities/Abilities/PlayerAbilityTauntInteractorScript.cs
+++ Assets/Scripts/Interactor/Abilities/Abilities/PlayerAbilityTauntInteractorScript.cs
@@ -7,13 +7,47 @@ public class PlayerAbilityTauntInteractorScript : MonoBehaviour, IAbilityScript
     public AudioClip soundClipTaunt;
     public AudioSource audioSource;
 
+    [SerializeField] private float duration = 1f;
+    [SerializeField] private Vector3 axis = Vector3.up;
+    private GameObject playerFace;
+
+    private float elapsed;
+    private bool isRotating;
+    private Quaternion initialRotation;
+
     public void Activate(GameObject face)
     {
-        audioSource.clip = soundClipTaunt;
-        if (audioSource != null && soundClipTaunt != null)
+        if (!isRotating)
         {
-            audioSource.Play();
+            audioSource.clip = soundClipTaunt;
+            duration = soundClipTaunt.length;
+            if (audioSource != null && soundClipTaunt != null)
+            {
+                audioSource.Play();
+            }
+
+            playerFace = face;
+            elapsed = 0f;
+            isRotating = true;
+            initialRotation = playerFace.transform.localRotation;
         }
     }
+
+    private void Update()
+    {
+        if (!isRotating || duration <= 0f)
+            return;
+
+        elapsed += Time.deltaTime;
+        float t = Mathf.Clamp01(elapsed / duration);
+        float angle = 360f * t;
+
+        // ÂÐÀÙÀÅÌ ÎÒÍÎÑÈÒÅËÜÍÎ ÑÂÎÅÉ ÎÑÈ
+        playerFace.transform.localRotation = initialRotation * Quaternion.AngleAxis(angle, axis.normalized);
+
+        if (t >= 1f)
+            isRotating = false;
+    }
+
 
 }
--- Assets/Scripts/Interactor/Abilities/PlayerAbilityInteractorScript.cs
+++ Assets/Scripts/Interactor/Abilities/PlayerAbilityInteractorScript.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class PlayerAbilityInteractorScript : MonoBehaviour
 {
+    public List<AbilityEntry> abilities;
     [SerializeField] private PlayerAbilityTauntInteractorScript tauntInteractor;
     [SerializeField] private PlayerMovementInteractorScript movementInteractor;
 
+    private void Awake() //REWRITE!!!!
+    {
+        abilities = new List<AbilityEntry>()
+        {
+            new() {
+                abilityScript = tauntInteractor,
+                type = AbilityType.Taunt
+            }
+        };
+    }
+
     public void ActivateAbility(AbilityType type)
     {
-        if (type == AbilityType.Taunt)
+        foreach (var ability in abilities)
         {
-            tauntInteractor.Activate(movementInteractor.GetCurrentFace());
+            if (ability.type == type)
+            {
+                ability.abilityScript.Activate(movementInteractor.GetCurrentFace());
+            }
         }
     }
+}
+
+public class AbilityEntry
+{
+    public IAbilityScript abilityScript;  
+    public AbilityType type;        
 }
