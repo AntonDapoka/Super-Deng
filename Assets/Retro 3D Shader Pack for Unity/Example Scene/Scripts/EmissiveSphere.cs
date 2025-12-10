f88b05b2c4762fa9587e24f75c6438fac5c51cde
Anton Borisov <antonimstudio@gmail.com>
2025-12-10 22:08:52 +0800

    Prefab updated

--- Assets/Retro 3D Shader Pack for Unity/Example Scene/Scripts/EmissiveSphere.cs
+++ Assets/Retro 3D Shader Pack for Unity/Example Scene/Scripts/EmissiveSphere.cs
@@ -1,9 +1,4 @@
-﻿//////////////////////////////////////////////////
-// Author:              LEAKYFINGERS
-// Date created:        04.11.19
-// Date last edited:    04.11.19
-//////////////////////////////////////////////////
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
