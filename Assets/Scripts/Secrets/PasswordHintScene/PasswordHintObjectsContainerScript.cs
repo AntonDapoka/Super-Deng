fbf4ebf4508cd59468cf51f3ab681f3e72738d9c
Anton Borisov <antonimstudio@gmail.com>
2025-12-22 18:44:03 +0800

    Script Folder Updated

--- Assets/Scripts/Secrets/PasswordHintScene/PasswordHintObjectsContainerScript.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PasswordHintObjectsContainerScript : MonoBehaviour
-{
-    [SerializeField] private GlowingPart[] arrayOfGlowingParts;
-}
