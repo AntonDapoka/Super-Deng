aee17e1d55e5c2d6369f59e84354d23d47ca1f41
AntonimStudio <antonimstudio@gmail.com>
2026-04-11 21:21:46 +0800

    Sorting files

---
+++ Assets/Scripts/Database/LevelSaveData.cs
@@ -0,0 +1,14 @@
+[System.Serializable]
+public class LevelSaveData 
+{
+    public int levelId;
+
+    public int score3Stars;
+    public int score2Stars;
+    public int score1Star;
+
+    public int scorePlayerLast;
+    public int scorePlayerBest;
+
+    public int difficulty;
+}
--- Assets/Scripts/MenuScripts/Interactor/ExitButtonScript.cs
+++ Assets/Scripts/MenuScripts/Interactor/ExitButtonScript.cs
@@ -17,8 +17,8 @@ public class ExitButtonScript : MonoBehaviour
             else if (holdTimer >= 1f && holdTimer <= 1.5f) textQuiting.text = "Quitting...";
             if (holdTimer >= holdTime)
             {
-                Application.Quit();
                 holdTimer = 0f;
+                Exit();
             }
         }
         else
